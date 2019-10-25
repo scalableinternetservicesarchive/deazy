497398a34ee2e1f614ae94bc6a0d1a974a7022be
Ali Mirabzadeh <ali.mz758@gmail.com>
2019-10-25 10:46:39 -0700

    URL redirection works

---
+++ deasy/app/controllers/landing_page_controller.rb
@@ -0,0 +1,5 @@
+class LandingPageController < ApplicationController
+    def home
+    end
+end
+  
--- deasy/config/routes.rb
+++ deasy/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'home_page#home'
   get 'home_page/home'
   get 'home_page/search', to: "home_page#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
