ffbab48721bd47dee8116cb892c14c957d4a3be6
jackvanboening <jackvanboening@gmail.com>
2019-10-31 10:59:24 -0700

    Add Save feature to Cards

--- deasy/app/controllers/home_page_controller.rb
+++ deasy/app/controllers/home_page_controller.rb
@@ -2,7 +2,14 @@ class HomePageController < ApplicationController
   def home
   end
   def search
-    @client = GooglePlaces::Client.new()
+    @client = GooglePlaces::Client.new("AIzaSyAJmcj9PCv7-_OtLh269hCBg32-H7xqxoA")
     @places = @client.spots_by_query(params[:places_option])
+  end
+  def save
+    @place = params[:place_id]
+    @saved_places = YAML.load(current_user.saved_places)
+    @saved_places.push(@place)
+    current_user.saved_places = @saved_places.to_yaml
+    current_user.save
   end
 end
