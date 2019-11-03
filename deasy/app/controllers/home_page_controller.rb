eb4672066423f785f1bf8d31f8a8c040ef601f44
jackvanboening <jackvanboening@gmail.com>
2019-11-02 17:28:34 -0700

    Fix saved places

--- deasy/app/controllers/home_page_controller.rb
+++ deasy/app/controllers/home_page_controller.rb
@@ -9,8 +9,12 @@ class HomePageController < ApplicationController
   end
   def save
     @place = params[:place_id]
-    @saved_places = YAML.load(current_user.saved_places)
-    @saved_places.push(@place)
+    if !current_user.saved_places.empty?
+      @saved_places = YAML.load(current_user.saved_places)
+      @saved_places.push(@place)
+    else 
+      @saved_places = [@place]
+    end
     current_user.saved_places = @saved_places.to_yaml
     current_user.save
   end
