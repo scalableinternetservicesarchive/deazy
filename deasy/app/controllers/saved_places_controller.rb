529669459cb21cb3b6cb757ff595e85c3628c28d
jackvanboening <jackvanboening@gmail.com>
2019-11-04 23:37:32 -0800

    Finish saved places page

--- deasy/app/controllers/saved_places_controller.rb
+++ deasy/app/controllers/saved_places_controller.rb
@@ -1,10 +1,16 @@
 class SavedPlacesController < ApplicationController
   def home
+    @client = GooglePlaces::Client.new(ENV["PLACES_API_KEY"])
+    @places_id = []
     if !current_user.saved_places.empty?
       @places_id = YAML.load(current_user.saved_places)
-    else 
-      @places = nil
     end
-    
+
+    @places = []
+    @places_id.each { |id| 
+      spot = @client.spot(id)
+      @places.push(spot)
+    }
+
   end
 end
