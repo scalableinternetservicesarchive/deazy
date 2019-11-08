fe29cc45b27c275df06bdd01998eb7a2f16f752d
ash <ashkan117@yahoo.com>
2019-11-08 10:51:17 -0800

    simple edit

--- deasy/app/controllers/home_page_controller.rb
+++ deasy/app/controllers/home_page_controller.rb
@@ -2,7 +2,7 @@ class HomePageController < ApplicationController
   def home
   end
   def search
-    @client = GooglePlaces::Client.new("AIzaSyDyvkPM-LAuAZ94T_hqcaWbof2bGNJyVQE")
+    @client = GooglePlaces::Client.new()
     @places = @client.spots_by_query(params[:places_option])
   end
 end
