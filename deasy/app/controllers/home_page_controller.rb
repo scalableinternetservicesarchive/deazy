ed81530f4d20426cb6369f0470ea7835c20be479
Ali Mirabzadeh <ali.mz758@gmail.com>
2019-11-10 15:39:31 -0800

    some UI improvement

--- deasy/app/controllers/home_page_controller.rb
+++ deasy/app/controllers/home_page_controller.rb
@@ -2,7 +2,7 @@ class HomePageController < ApplicationController
   def home
   end
   def search
-    @client = GooglePlaces::Client.new("AIzaSyBVRnJTDFMf-3q3qrjjIP5NVnriZa9uDYQ")
+    @client = GooglePlaces::Client.new("KEY")
     if params[:search] 
 
       @places = @client.spots_by_query( params[:search] +" near "+ params[:city])
