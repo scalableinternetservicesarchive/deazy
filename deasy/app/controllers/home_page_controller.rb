d9ff1e834fa50cc89e4fcda426dad61d870fdbf5
Ali Mirabzadeh <ali.mz758@gmail.com>
2019-11-13 18:36:04 -0800

    more sort options- sort(distance) doesn't quite workgit add .

--- deasy/app/controllers/home_page_controller.rb
+++ deasy/app/controllers/home_page_controller.rb
@@ -7,6 +7,15 @@ class HomePageController < ApplicationController
       if params[:sortCat] == "Rating"
       @places = @client.spots_by_query( params[:search] +" near "+ params[:city]).sort_by {|m| -m.rating}
       end
+      if params[:sortCat] == "Name"
+        @places = @client.spots_by_query( params[:search] +" near "+ params[:city]).sort_by {|m| -m.name}
+      end
+      if params[:sortCat] == "Distance"
+        cityCoordinates = Geocoder.search("172.56.21.89")
+        @places = @client.spots_by_query( params[:search] +" near "+ params[:city]).sort_by {|m| 
+        -Geocoder::Calculations.distance_between(cityCoordinates.first.coordinates, [m.lng,m.lat])
+      }
+      end
     end
   end
   def save
