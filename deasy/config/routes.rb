6b845f7bdc1200a03c464228b6b04d72d69eedf5
jackvanboening <jackvanboening@gmail.com>
2019-12-01 17:21:55 -0800

    Add feature to remove place

--- deasy/config/routes.rb
+++ deasy/config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   resources :reviews
   post 'home_page/save'
   get 'home_page/details'
+  post 'saved_places/remove'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
